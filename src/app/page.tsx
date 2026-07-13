9e42121fdf9e8f844d59275f1c96fda82c40f115
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-07-13 11:21:36 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="grow">
-      <h1>Tela Inicial</h1>
+      <div className="container mx-auto">
+        {/*seção hero*/}
+        <section>
+          <h1>Receitas Deliciosas</h1>
+          <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+
+          <Link href="/receitas">
+            Ver todas as receitas
+          </Link>
+        </section>
+      </div>
     </main>
   );
 }
