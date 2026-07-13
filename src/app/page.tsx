26d61cbb3fb4f75c9eba83a270b68a726fc4e66d
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-07-13 12:11:20 -0300

    feat: adicionando estilização da seção receitas em destaques

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,13 +17,13 @@ export default function Home() {
       </section>
 
       {/*seção receitas em destaque*/}
-      <section>
-        <div className="container mx-auto">
-          <h2>Receitas em destaque</h2>
+      <section className="py-12">
+        <div className="flex flex-col items-center container mx-auto">
+          <h2 className="text-lg font-bold">Receitas em destaque</h2>
 
           {/*cards de receita*/}
 
-          <Link href="/receitas">
+          <Link className="flex text-orange-400 hover:text-orange-700 transition-colors" href="/receitas">
             Ver todas as receitas
             <ChevronRight />
           </Link>
