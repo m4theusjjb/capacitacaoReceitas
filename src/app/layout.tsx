a8b1a323cacb1936d90d0e39f1bfb0b0c5a7ce63
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-07-13 10:20:24 -0300

    feat: estrutura do footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <footer>
+            <div>
+                <p>© ( ͡° ͜ʖ ͡°) 2026 Receitas Deliciosas. Todos os direitos reservados</p>
+            </div>
+        </footer>
+    )
+}
