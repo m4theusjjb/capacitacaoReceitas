6b16cad5e7c01471a02b141ee71e1af51d2df7b9
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-07-13 11:37:47 -0300

    style: adiconando hover no botão de receitas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
           <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
 
-          <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2" href="/receitas">
+          <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
             Ver todas as receitas
           </Link>
         </div>
