ce7a816dcc6290b00b806aaa17ba0329949b1ab5
Matheus Junqueira Jundurian Bolonha <tejunqueira22@gmail.com>
2026-07-14 11:08:02 -0300

    feat: estilização da seção de infos da receita

--- src/app/receitas/[id]/page.tsx
+++ src/app/receitas/[id]/page.tsx
@@ -1,45 +1,70 @@
 import { ChevronLeft } from "lucide-react"
 import Link from "next/link"
 import Image from "next/image"
+import { recipes } from "@/lib/data";
+import { notFound } from "next/navigation";
+interface RecipesPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
 
-export default function ReceitaPage(){
+export default async function ReceitasPage({ params }: RecipesPageProps) {
+  const { id } = await params;
+  const recipe = recipes.find((recipe) => recipe.id === id);
+
+  if (!recipe) {
+    return notFound();
+  }
+
     return  (
         <main className="grow py-8">
             <div className="container mx-auto">
-                <Link className = "flex text-orange-500 hover:text-orange-700" href="receitas">
+                <Link className = "flex text-orange-500 hover:text-orange-700 mb-6" href="receitas">
                     <ChevronLeft />
                     Voltar para receitas
                 </Link>
 
-                <section>
+                <section className="rounded-lg overflow-hidden shadow-md">
                     {/* Imagem da capa da receita */}
                     <div className="relative h-96 w-full">
                         <Image 
-                            src={}
+                            src={recipe.image}
                             fill
-                            alt="Título da receita"
+                            alt={recipe.title}
+                            className="object-cover"
                         />
                     </div>
 
                     {/*Descrição da receita*/}
-                    <div>
-                        <h1>Título da receita</h1>
-                        <p>Descrição</p>
+                    <div className="flex flex-col gap-6 p-6">
+
+                        {/* Título de descrição */}
+                        <div>
+                            <h1 className="text-3xl font-bold">{recipe.title}</h1>
+                            <p>{recipe.description}</p>
+                        </div>
 
                         {/* Infos de preparo*/}
                         <div>
                             {/* TO DO: componentes de info */}
-
                         </div>
 
                         {/*Colunas*/}
-                        <div>
+                        <div className="grid grid-cols-2">
                             {/* Coluna dos ingredientes */}
                             <div>
-                            
+                                <h2 className="text-xl font-bold mb-4">Ingredientes</h2>
+                                <ul className="list-disc list-inside space-y-2">
+                                    {recipe.ingredients.map((ingredient)=>(
+                                        <li className="marker:text-orange-500">{ingredient}</li>
+                                    ))}
+                                </ul>
                             </div>
+
                             {/*Coluna do modo de preparo*/}
                             <div>
+                                <h2 className="text-xl font-bold mb-4">Modo de Preparo</h2>
                                 {/*TO DO: componente de modo de preparo*/}
                             </div>
                         </div>
